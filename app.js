c312709e47350cf5b559c55c1f266b036eb20551
Mukesh M <me@mukeshm.com>
2018-02-23 16:52:32 +0530

    Add master slave logic

--- app.js
+++ app.js
@@ -5,23 +5,50 @@ const server = require('http').Server(app)
 
 const port = 9000
 const io = require('socket.io')(server)
-
 app.use(express.static('public'))
+
+const buffers = {}
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-// FIXME
-app.get('/test', function (req, res) {
+app.get('/buffer/:bufferid', function (req, res) {
+  console.log(req.params.bufferid)
   res.sendFile(path.join(__dirname, '/view/index.html'))
 })
 
 io.on('connection', function (socket) {
   console.log('a user connected')
-  socket.emit('server', {msg: 'hello from server'})
-  socket.on('client', function (data) {
+  // handle disconnects
+  // when disconnected  remove user from buffers
+  socket.on('handshake', function (data) {
     console.log(data)
+    data = JSON.parse(data)
+    const bufferid = data.bufferid
+    const userid = data.userid
+    if (buffers[bufferid]) {
+      if (buffers[bufferid].slave) {
+        console.log('maximum users reached ', userid, buffers)
+        socket.emit('handshake_error', JSON.stringify({msg: 'maximum users reached'}))
+      } else {
+        const master = buffers[bufferid].master
+        if (master.userid === userid) {
+          socket.emit('handshake_error', JSON.stringify({msg: 'you are already a master of the 9 realms'}))
+        } else {
+          console.log('adding slave ', userid, buffers)
+          buffers[bufferid].slave = {userid: userid, socket: socket}
+          // send slave id to master
+          master.socket.emit('handshake', JSON.stringify({...data, from: 'slave'}))
+          // send master id to slave
+          socket.emit('handshake', JSON.stringify({userid: master.userid, bufferid: bufferid, from: 'master'}))
+        }
+      }
+    } else {
+      // set master
+      buffers[bufferid] = {}
+      buffers[bufferid].master = {userid: userid, socket: socket}
+    }
   })
 })
 
--- public/js/client.js
+++ public/js/client.js
@@ -1,15 +1,19 @@
 const socket = io.connect()
-socket.on('server', function (data) {
+
+socket.on('handshake', function (data) {
   console.log(data)
-  socket.emit('client', {msg: 'hello from client'})
+  // if master start webrtc peerconnection
 })
 
+socket.on('handshake_error', function (data) {
+  console.log(data)
+  // show a notification
+})
 
 const connectButton = document.getElementById('connectButton')
 connectButton.addEventListener('click', getUsername)
 
 function getUsername (e) {
   const username = document.getElementById('username').value
-  console.log(username)
-  socket.emit('client', {username: username, path: window.location.pathname})
+  socket.emit('handshake', JSON.stringify({userid: username, bufferid: window.location.pathname}))
 }
